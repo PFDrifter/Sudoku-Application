f53aa17bfee081ba4e2e67a9cd0caf4b97609d8c
Default Profile <terpstra@ecuador.cs.lan>
2016-01-25 08:45:29 -0400

    Initial Commit

---
+++ lab3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lab3
+//
+//  Created by Jennifer Terpstra on 2016-01-25.
+//  Copyright © 2016 Jennifer Terpstra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
